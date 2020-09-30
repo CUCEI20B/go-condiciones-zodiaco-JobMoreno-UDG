0f959aae4c8080aa2fc89e1d16772a5af8a6d9b0
JobMoreno-UDG <job.moreno7426@alumnos.udg.mx>
2020-09-30 15:24:51 -0500

    zodiaco 2.0

--- main.go
+++ main.go
@@ -2,35 +2,35 @@ package main
 
 import "fmt"
 
-func main()  {
+func main() {
 	var mes, dia int
 	fmt.Print("Introduce tu dia de nacimiento ")
 	fmt.Scan(&dia)
 	fmt.Print("Introduce tu mes de nacimiento ")
 	fmt.Scan(&mes)
-	if (dia >=22  && mes==12 ) || (dia <=20  && mes==1){
+	if (dia >= 22 && mes == 12) || (dia <= 20 && mes == 1) {
 		fmt.Print("capricornio")
-	}else if (dia >=21  && mes==2 ) || (dia <=19  && mes==2){
+	} else if (dia >= 21 && mes == 2) || (dia <= 19 && mes == 2) {
 		fmt.Print("acuario")
-	}else if (dia >=20  && mes==2 ) || (dia <=20  && mes==3){
+	} else if (dia >= 20 && mes == 2) || (dia <= 20 && mes == 3) {
 		fmt.Print("piscis")
-	}else if (dia >=21  && mes==3 ) || (dia <=20  && mes==4){
+	} else if (dia >= 21 && mes == 3) || (dia <= 20 && mes == 4) {
 		fmt.Print("aries")
-	}else if (dia >=21  && mes==4 ) || (dia <=21  && mes==5){
+	} else if (dia >= 21 && mes == 4) || (dia <= 21 && mes == 5) {
 		fmt.Print("tauro")
-	}else if (dia >=22  && mes==5 ) || (dia <=21  && mes==6){
+	} else if (dia >= 22 && mes == 5) || (dia <= 21 && mes == 6) {
 		fmt.Print("geminis")
-	}else if (dia >=22  && mes== 6 ) || (dia <=23  && mes==7){
+	} else if (dia >= 22 && mes == 6) || (dia <= 23 && mes == 7) {
 		fmt.Print("cancer")
-	}else if (dia >=24  && mes==7 ) || (dia <=23  && mes==8){
+	} else if (dia >= 24 && mes == 7) || (dia <= 23 && mes == 8) {
 		fmt.Print("leo")
-	}else if (dia >=24  && mes==8 ) || (dia <=23  && mes==9){
+	} else if (dia >= 24 && mes == 8) || (dia <= 23 && mes == 9) {
 		fmt.Print("virgo")
-	}else if (dia >=24  && mes==9 ) || (dia <=23  && mes==10){
+	} else if (dia >= 24 && mes == 9) || (dia <= 23 && mes == 10) {
 		fmt.Print("libra")
-	}else if (dia >=24  && mes==10 ) || (dia <=22  && mes==11){
+	} else if (dia >= 24 && mes == 10) || (dia <= 22 && mes == 11) {
 		fmt.Print("escorpio")
-	}else if (dia >=22  && mes==11 ) || (dia <=21  && mes==12){
+	} else if (dia >= 23 && mes == 11) || (dia <= 21 && mes == 12) {
 		fmt.Print("sagitrio")
 	}
 }
